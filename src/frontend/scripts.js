9ae589fb0e11b0dc095dafddf242b1f6d1ef7146
lisafritsche <lisa.fritsche@mein.gmx>
2025-05-16 21:36:52 +0200

    Add files via upload

--- src/frontend/scripts.js
+++ src/frontend/scripts.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector(".card-button");
+  const card = document.getElementById("flip-card");
 
+  button.addEventListener("click", () => {
+    card.classList.toggle("flipped");
+  });
+});
